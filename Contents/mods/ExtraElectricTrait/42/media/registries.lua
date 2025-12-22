a686b91919f71c21ca0438e7efd79a3520468df7
Vico <vpontin93@gmail.com>
2025-12-22 17:33:27 -0300

    Revert "Tentativa de atualizar mod para 42.13"
    This reverts commit 15396f5cdd873d43f5d894b93c5bf3785ee23806.

--- Contents/mods/ExtraElectricTrait/42/media/registries.lua
+++
@@ -1,5 +0,0 @@
-_ExtraElectricTrait = {}
-_ExtraElectricTrait.traits = {
-    Geek = CharacterTrait.register("extraelectric:geek"),
-    Tinkerer = CharacterTrait.register("extraelectric:tinkerer")
-}
